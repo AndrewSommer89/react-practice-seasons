46f657e09cb25b2459aecd37a9fae64e2d34b1b0
Andrew Sommere <andrew.conrad.sommer@gmail.com>
2019-08-30 15:33:11 -0500

    lattitude is shown on screen

---
+++ src/index.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Seasons from "./Seasons";
+
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        //initialize the state
+        this.state = { 
+            lat: null,
+            errorMessage: '',
+        };
+
+        window.navigator.geolocation.getCurrentPosition(
+            //If we are able to get users location
+            position => {
+                //Update the state of "lat"
+                this.setState( { lat: position.coords.latitude } )
+            },
+            //If we are unable to get users location
+            (err) => { 
+                this.setState({ errorMessage: err.message});
+             }
+        );
+    }
+
+    render() {
+        //Error when unablle to find "lat"
+        if(this.state.errorMessage && !this.state.lat){
+            return <div> Error: {this.state.errorMessage} </div>
+        }
+        //Lat is found
+        if(!this.state.errorMessage && this.state.lat){
+            return <div>Latitude: {this.state.lat}</div>
+        }
+        return <div>Loading...</div>
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    document.querySelector("#root")
+)
